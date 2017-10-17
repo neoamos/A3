307cb5b5fc70969580c066917983f279c41f0a99
hirominami <hirominam95@gmail.com>
2017-10-17 18:02:15 -0400

    message

--- translator.ml
+++ translator.ml
@@ -610,8 +610,16 @@ and ast_ize_expr_tail (lhs:ast_e) (tail:parse_tree) : ast_e =
   | _ -> raise (Failure "malformed parse tree in ast_ize_expr_tail")
 ;;
 
+
 let pt = parse ecg_parse_table sum_ave_prog;;
-ast_ize_P pt;;
+let ast = ast_ize_P pt;;
+
+
+
+
+
+
+
 (*******************************************************************
     Translate to C
  *******************************************************************)
@@ -623,28 +631,94 @@ ast_ize_P pt;;
    indicating their names and the lines on which the writes occur.  Your
    C program should contain code to check for dynamic semantic errors. *)
 
-(*  commented out so this code will complile
+  (*commented out so this code will complile*)
 
-let rec translate (ast:ast_sl)
-    :  string *  string
-    (* warnings  output_program *) = ...
 
-and translate_sl (...
 
-and translate_s (...
 
-and translate_assign (...
 
-and translate_read (...
 
-and translate_write (...
 
-and translate_if (...
 
-and translate_do (...
 
-and translate_check (...
 
-and translate_expr (...
 
-*)
+
+
+
+let rec translate (ast:ast_sl) =
+    let ccode = 
+    [
+    "#include <stdio.h>";
+    "#include <stdlib.h>";
+    "";
+    "int getint() {";
+    "int a;";
+    "if(scanf(\"%d\", &a)>0){";
+    "return a;";
+    "} else {";
+    "printf(\"Input must be integer.\");";
+    "return;";
+    "}";
+    "}";
+    "";
+    "void putint(int n) {";
+    "printf(\"%d\\n\", n);";
+    "}";
+    "";
+    ] in
+
+    translate_sl ast
+    
+
+
+
+and translate_sl (ast:ast_sl) =
+    match ast with
+    | h::t -> translate_s h @ translate_sl t
+    | h::[] -> translate_s h 
+    | [] -> ""
+
+and translate_s (ast:ast_s) =
+    match ast with
+    | AST_assign a -> traslate_assign a
+    | AST_read a -> translate_read a
+    | AST_write a -> translate_write a
+    | AST_if a -> translate_if a
+    | AST_do a -> translate_do a
+    | AST_check a -> translate_check a
+    | AST_error a -> "Syntax Error."
+
+
+and translate_assign (ast:string*ast_e) =
+    let (s,e) = ast in
+    "int " @ s @ " = " @ translate_expr e @";\n"
+
+and translate_read (ast:string) =
+    ast @ " = getint(); \n" 
+
+and translate_write (ast:ast_e) =
+    "putint(" @ traslate_expr ast @ ");\n"
+
+and translate_if (ast:ast_e*ast_sl) =
+    let (e,sl) = ast in 
+    "if(" @ translate_expr e @ ") { \n" @ translate_sl sl @"\n}\n" 
+
+and translate_do (ast:ast_sl) =
+    "while(true){\n" + translate_sl ast +"\n}\n"
+
+and translate_check (ast:ast_e) =
+    "if(!" @ translate_expr ast @ ") {break;} \n"
+
+and translate_expr (ast:ast_e) =
+    match ast with
+    | AST_binop (op,e1,e2) -> translate_expr e1 @ op @ translate_expr e2
+    | AST_id a -> a
+    | AST_num a -> a
+
+
+
+
+
+
+translate ast;;
